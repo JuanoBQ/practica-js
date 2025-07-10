78b7fda5398bbee69450a3a00d51278a81bd5a96
Juan Jose Bravo Quiroga <juanjo.bravo3@gmail.com>
2025-07-10 21:25:19 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const readline = require('readline-sync');
+
+
+let nombre = "Juan Jose";
+let edad = 25;
+let esEstudiante = true;
+
+let sinValor 
+let valorNulo = null;
+console.log("sinValor: ", sinValor)
+console.log("sinValor: ", valorNulo)
+
+let tuNombre = readline.question("Ingresa tu nombre: ");
+console.log("Hola, " + tuNombre);
